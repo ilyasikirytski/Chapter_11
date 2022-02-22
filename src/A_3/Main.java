feb2f9ca17f3fc3513ec045e7c59955604a7db6b
Ilya Sikirytski <ilyasikirytski@gmail.com>
2022-02-22 17:54:53 +0300

    A_3 - refactor thread startring

--- src/A_3/Main.java
+++ src/A_3/Main.java
@@ -6,15 +6,25 @@
  */
 package A_3;
 
+import A_4.CallCenter;
+
 public class Main {
     public static void main(String[] args) {
         Parking parking = new Parking();
 
-        new Thread(new Car(parking, "1")).start();
-        new Thread(new Car(parking, "2")).start();
-        new Thread(new Car(parking, "3")).start();
-        new Thread(new Car(parking, "4")).start();
-        new Thread(new Car(parking, "5")).start();
+//        new Thread(new Car(parking, "1")).start();
+//        new Thread(new Car(parking, "2")).start();
+//        new Thread(new Car(parking, "3")).start();
+//        new Thread(new Car(parking, "4")).start();
+//        new Thread(new Car(parking, "5")).start();
 
+        for (int i = 0; i < 5; i++){
+            try {
+                Thread.sleep(1000);
+                new Thread(new Car(parking, "" + i)).start();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
