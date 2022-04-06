66d4e94bdfa882ff5a707220ea9a1005b5020bb4
Ilya Sikirytski <ilyasikirytski@gmail.com>
2022-04-06 11:09:08 +0300

    A_3 parking interface create

--- src/A_3/sync/Parking.java
+++ src/A_3/sync/Parking.java
@@ -1,11 +1,12 @@
 package A_3.sync;
 
-public class Parking {
+public class Parking implements ParkingFun {
     private final boolean[] parkingPlaces = new boolean[5];
 
     public Parking() {
     }
 
+    @Override
     public void park(Car car) {
         boolean allTaken = false;
         for (int i = 0; i < parkingPlaces.length; i++) {
@@ -17,12 +18,13 @@ public class Parking {
             }
         }
         if (allTaken) {
-            System.out.printf("auto %s can't park - all parks are taken", car.carName);
+            System.out.printf("auto %s can't park - all parks are taken\n", car.carName);
         } else {
             System.out.printf("auto %s - parked on place %s\n", car.carName, car.parkNumber);
         }
     }
 
+    @Override
     public void unPark(Car car) {
         parkingPlaces[car.parkNumber] = false;
         System.out.printf("auto %s - gone form place %s\n", car.carName, car.parkNumber);
---
+++ src/A_3/sync/ParkingFun.java
@@ -0,0 +1,6 @@
+package A_3.sync;
+
+public interface ParkingFun {
+    void park(Car car);
+    void unPark(Car car);
+}
