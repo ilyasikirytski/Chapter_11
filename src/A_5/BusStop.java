f1b8b7f210dc490e72d3f390f5182958bd1e2084
Ilya Sikirytski <ilyasikirytski@gmail.com>
2022-04-24 19:35:45 +0300

    A_5 - create new method "park()"

--- src/A_5/BusStop.java
+++ src/A_5/BusStop.java
@@ -1,11 +1,25 @@
 package A_5;
 
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.TimeUnit;
+
 public class BusStop {
     public String busStopName;
-    public int countOfBuses;
+    private final Semaphore semaphore;
 
-    public BusStop(String busStopName, int countOfBuses) {
+    public BusStop(String busStopName, int maxCountOfBusesOnBusStop) {
         this.busStopName = busStopName;
-        this.countOfBuses = countOfBuses;
+        this.semaphore = new Semaphore(maxCountOfBusesOnBusStop);
+    }
+
+    public void park(String busNumber) throws InterruptedException {
+        if (semaphore.tryAcquire(2000, TimeUnit.MILLISECONDS)) {
+            System.out.printf("%s заехал на %s - загрузка\n", busNumber, busStopName);
+            Thread.sleep(4000);
+            System.out.printf("%s покинул %s\n", busNumber, busStopName);
+        } else {
+            System.out.printf("%s не смог заехать на %s - все места заняты\n", busNumber, busStopName);
+        }
+        semaphore.release();
     }
 }
--- src/A_5/Route.java
+++ src/A_5/Route.java
@@ -1,37 +1,22 @@
 package A_5;
 
-import java.util.concurrent.Semaphore;
-import java.util.concurrent.TimeUnit;
-
 public class Route {
-    private final Semaphore semaphore;
     private final BusStop[] busStops;
 
     Route(int countOfBusStops, int maxCountOfBusesOnBusStop) {
-        this.semaphore = new Semaphore(countOfBusStops);
         this.busStops = new BusStop[countOfBusStops];
         for (int i = 0; i < countOfBusStops; i++) {
             this.busStops[i] = new BusStop("" + i, maxCountOfBusesOnBusStop);
         }
     }
 
+    /*
+    написать метод park() на остановке. который будет лочить остановку(2 permits)
+     */
     public void drive() throws InterruptedException {
         String busNumber = Thread.currentThread().getName();
-        int busStopNumber = 0;
-        if (semaphore.tryAcquire(4000, TimeUnit.MILLISECONDS)) {
-            for (int i = 0; i < busStops.length; i++) {
-                if (busStops[i].countOfBuses > 0) {
-                    busStops[i].countOfBuses -= 1;
-                    System.out.printf("%s заехал на %s - загрузка\n", busNumber, i);
-                    busStopNumber = i;
-                    Thread.sleep(2000);
-                    busStops[busStopNumber].countOfBuses += 1;
-                    System.out.printf("%s покинул %s\n", busNumber, i);
-                } else {
-                    System.out.printf("%s нe смог заехать на %s - все места заняты\n", busNumber, i);
-                }
-            }
-            semaphore.release();
+        for (BusStop busStop : busStops) {
+            busStop.park(busNumber);
         }
     }
 }
