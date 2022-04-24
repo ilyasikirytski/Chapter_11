8735bca188637c1b33a588869b4561e1d0b3f8c8
Ilya Sikirytski <ilyasikirytski@gmail.com>
2022-04-24 19:49:25 +0300

    A_1\new_version - implements constant int - to avoid "magic numbers"

--- src/A_1/new_version/Main.java
+++ src/A_1/new_version/Main.java
@@ -2,8 +2,8 @@ package A_1.new_version;
 
 public class Main {
     public static void main(String[] args) {
-        Port port = new Port(5, 50);
-        Port port2 = new Port(5, 0);
+        Port port = new Port(5, 50, 5);
+        Port port2 = new Port(5, 0, 5);
 
         for (int i = 0; i < 5; i++) {
             new Ship("" + i, 10, port, port2).start();
--- src/A_1/new_version/Port.java
+++ src/A_1/new_version/Port.java
@@ -5,12 +5,14 @@ import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicIntegerArray;
 
 public class Port implements PortFun {
+    private static final int BERTH_FREE = 0;
+    private static final int BERTH_TAKEN = 1;
     private final Semaphore semaphore;
     private final AtomicIntegerArray portBerths;
     public AtomicInteger countOfContainers;
 
-    public Port(int shipCapacity, int countOfContainers) {
-        this.portBerths = new AtomicIntegerArray(5);
+    public Port(int shipCapacity, int countOfContainers, int countOfBerth) {
+        this.portBerths = new AtomicIntegerArray(countOfBerth);
         this.semaphore = new Semaphore(shipCapacity);
         this.countOfContainers = new AtomicInteger(countOfContainers);
     }
@@ -21,14 +23,14 @@ public class Port implements PortFun {
         int berthNumber = 0;
         semaphore.acquire();
         for (int i = 0; i < portBerths.length(); i++) {
-            if (portBerths.compareAndSet(i, 0, 1)) {
+            if (portBerths.compareAndSet(i, BERTH_FREE, BERTH_TAKEN)) {
                 berthNumber = i;
                 break;
             }
         }
         System.out.printf("p1 / s%s / b%s: load %s-containers/ countOfContainers-p1-%s\n",
                 shipName, berthNumber, shipContainerCapacity, this.countOfContainers.addAndGet(-shipContainerCapacity));
-        portBerths.set(berthNumber, 0);
+        portBerths.set(berthNumber, BERTH_FREE);
         System.out.printf("p1 / s%s / b%s: go to port2\n", shipName, berthNumber);
         semaphore.release();
     }
@@ -40,14 +42,14 @@ public class Port implements PortFun {
         semaphore.acquire();
         int berthNumber = 0;
         for (int i = 0; i < portBerths.length(); i++) {
-            if (portBerths.compareAndSet(i, 0, 1)) {
+            if (portBerths.compareAndSet(i, BERTH_FREE, BERTH_TAKEN)) {
                 berthNumber = i;
                 break;
             }
         }
         System.out.printf("p2 / s%s / b%s: UnLoad %s containers/ countOfContainers-p2-%s\n",
                 shipName, berthNumber, shipContainerCapacity, this.countOfContainers.addAndGet(shipContainerCapacity));
-        portBerths.set(berthNumber, 0);
+        portBerths.set(berthNumber, BERTH_FREE);
         semaphore.release();
     }
 }
