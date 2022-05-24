aa8714e591c5e9125ff8a8941bf1d353e04effe1
Ilya Sikirytski <ilyasikirytski@gmail.com>
2022-05-24 03:05:49 +0300

    A_2 task done\refactor ArrayList initialize

--- src/A_2/Reader.java
+++ src/A_2/Reader.java
@@ -1,19 +1,13 @@
 package A_2;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.concurrent.CopyOnWriteArrayList;
 
 public class Reader extends Thread {
     private final Library library;
     private final CopyOnWriteArrayList<Book> readerBooks = new CopyOnWriteArrayList<>();
 
-    /*
-    коллекция должна быть private final
-        написать методы взять книгу
-        отдать книгу
-
-        в метод returnBook передать только список книг а не всего читателя
-     */
     public void addBooks(Book book) {
         readerBooks.add(book);
     }
@@ -26,14 +20,8 @@ public class Reader extends Thread {
     @Override
     public void run() {
         try {
-            ArrayList<String> namesOfBook = new ArrayList<>();
-            namesOfBook.add("LibraryBook1");
-            namesOfBook.add("LibraryBook2");
-            namesOfBook.add("HomeBook1");
-            namesOfBook.add("HomeBook2");
-
             System.out.printf("%s пришёл в билитотеку\n", Thread.currentThread().getName());
-            library.takeBook(this, namesOfBook);
+            library.takeBook(this, new ArrayList<>(Arrays.asList("LibraryBook1", "LibraryBook2", "HomeBook1", "HomeBook2")));
             library.returnBook(readerBooks);
         } catch (Exception e) {
             e.printStackTrace();
